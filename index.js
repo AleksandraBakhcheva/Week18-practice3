13dd1d1d03e160b3d2e56c1e4864ad8a72b42937
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-08-06 15:08:17 +0500

    files added

---
+++ index.js
@@ -0,0 +1,44 @@
+let container = document.createElement("form");
+container.classList.add("container");
+container.innerHTML += `<div class="result"></div>
+<label for="messages"><input class="nickname" type="text" id="messages" placeholder="Имя" /><input class="picture"  type="url" id="messages" placeholder="Здесь можно вставить ссылку на аватарку" /><input class="messages" type="text" id="messages" placeholder="Введите сообщение" /></label>
+<button type="submit">Отправить</button>`;
+document.body.append(container);
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addMessage();
+});
+
+function addMessage() {
+
+    let nickName = document.querySelector(".nickname").value;
+    if (nickName !== "") {
+        localStorage.setItem('name', nickName);
+    }
+
+    let nickAvatar = document.querySelector(".picture").value;
+    if (nickAvatar !== "") {
+        let image = document.createElement("img");
+        image.classList.add("nickAvatar");
+        image.src = `${nickAvatar}`;
+        document.querySelector(".result").appendChild(image);
+        localStorage.setItem('avatar', nickAvatar);
+    }
+
+    let message = document.querySelector(".messages").value;
+    
+    if (message !== "") {
+        checkSpam(message);
+        document.querySelector(".messages").value = "";
+    }
+    else {
+        return 0;
+    }
+
+    function checkSpam(str) {
+        let finalstr = str.replace(/viagra|XXX/gi, "***");
+        document.querySelector(".result").innerHTML += nickName + ": " + finalstr + "<br/>";
+    }
+}
